a3cd2cd3d4923335d8fa8eeb72b632df7731512e
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-30 01:03:20 +0300

    Задача 41 решена 2 спосабами второй спомощью рекурсии

---
+++ 1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+void positiveNumbers()
+{
+    string x = "";
+    int count = 0;
+    Console.WriteLine("Чтобы завершить программу нажмите 'Enter'");
+    while (true)
+    {
+        try
+        {
+            Console.WriteLine("Ведите число:");
+            x = Console.ReadLine();
+            if (x == "")
+            {
+                break;
+            }
+            int num = Convert.ToInt32(x);
+            if (num > 0)
+            {
+                count++;
+            }
+        }
+        catch
+        {
+            Console.WriteLine($"{x} - это не число!");
+            continue;
+        }
+    }
+
+    Console.WriteLine($"Положителных чисел: {count}");
+
+}
+
+positiveNumbers();
+
+//Рекурсия
+int positiveNumbers2()
+{
+    int count = 0;
+    Console.Write("Ведите число: ");
+    string x = Console.ReadLine();
+    if (x == "")
+    {
+        return count;
+    }
+    else
+    {
+        if (Convert.ToInt32(x) > 0) count++;
+        return positiveNumbers2() + count;
+    }
+}
+Console.WriteLine($" positive numbers - {positiveNumbers2()}");
